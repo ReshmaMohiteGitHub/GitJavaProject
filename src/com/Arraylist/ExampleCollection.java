acea6f880bbc1dca948e934e91e067cb045dcb94
ReshmaMohiteGitHub <reshmamohite06@gmail.com>
2022-12-25 13:58:36 +0530

    first commit: arraylist code

---
+++ src/com/Arraylist/ExampleCollection.java
@@ -0,0 +1,14 @@
+package com.Arraylist;
+
+import java.util.ArrayList;
+
+public class ExampleCollection {
+	public static void main(String [] args) {
+		ArrayList<String>list = new ArrayList<String>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		System.out.println(list);
+	}
+
+}
